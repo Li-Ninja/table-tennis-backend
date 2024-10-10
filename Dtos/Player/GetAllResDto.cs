4226b6bed43b4eae2156edb1f52d3b001a2cae16
Li-Ninja <lininja.tw@gmail.com>
2024-10-10 16:31:22 +0800

    feat: player rank

--- Dtos/Player/GetAllResDto.cs
+++ Dtos/Player/GetAllResDto.cs
@@ -4,6 +4,7 @@ public class GetAllResDto
     public int Id { get; set; }
     public required string Name { get; set; }
     public int Score { get; set; }
+    public int Rank { get; set; }
     public int ResultCount { get; set; }
     public int WinningCount { get; set; }
     public DateTimeOffset? UpdateDateTime { get; set; }
